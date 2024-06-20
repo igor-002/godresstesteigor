f518f2d609b1a03aa2fcedbe134dd4407853f63b
Lucas Pedro Chaves <lucaspedrosupergeeks@gmail.com>
2024-06-20 18:23:40 -0300

    React-hook-forms e Yup, Formulário de cadastro de usuários criado

--- src/app/user/register.tsx
+++ src/app/user/register.tsx
@@ -1,9 +1,115 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { useForm, Controller, SubmitHandler } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+type FormData = {
+    name: string;
+    surname: string;
+    email: string;
+    password: string;
+}
+
+const registerSchema = yup.object({
+    name: yup.string().required('Nome é obrigatório'),
+    surname: yup.string(),
+    email: yup.string().email('Email inválido').required('Email é obrigatório'),
+    password: yup.string()
+        .required('Senha é obrigatória')
+        .min(8, 'Senha deve ter pelo menos 8 caracteres')
+        .matches(/[A-Z]/, "A senha deve conter pelo menos uma letra maiúscula")
+        .matches(/[0-9]/, "A senha deve conter pelo menos um número")
+        .matches(/[!@#$%^&*(),.?":{}|<>_-]/, "A senha deve conter pelo menos um carctere especial")
+}).required();
 
 export default function Register() {
+    const form = useForm<FormData>({
+        defaultValues: {
+            name: "",
+            surname: "",
+            email: "",
+            password: ""
+        },
+        resolver: yupResolver(registerSchema),
+    });
+
+    const { handleSubmit, control, formState: { errors } } = form;
+
+    const onSubmit: SubmitHandler<FormData> = (data) => {
+        console.log(data);
+    };
+
     return (
         <View style={styles.container}>
-        
+            <Controller
+                control={control}
+                name="name"
+                render={({ field: { value, onChange } }) => (
+                    <>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Nome"
+                            onChangeText={onChange}
+                            value={value}
+                        />
+                        {errors.name && <Text style={styles.error}>{errors.name.message}</Text>}
+                    </>
+                )}
+            />
+            <Controller
+                control={control}
+                name="surname"
+                render={({ field: { value, onChange } }) => (
+                    <>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Sobrenome"
+                            onChangeText={onChange}
+                            value={value}
+                        />
+                        {errors.surname && <Text style={styles.error}>{errors.surname.message}</Text>}
+                    </>
+                )}
+            />
+            <Controller
+                control={control}
+                name="email"
+                render={({ field: { value, onChange } }) => (
+                    <>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={onChange}
+                            placeholder="Email"
+                            value={value}
+                            autoCapitalize="none"
+                        />
+                        {errors.email && <Text style={styles.error}>{errors.email.message}</Text>}
+                    </>
+                )}
+            />
+            <Controller
+                control={control}
+                name="password"
+                render={({ field: { value, onChange } }) => (
+                    <>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Senha"
+                            onChangeText={onChange}
+                            value={value}
+                            secureTextEntry={true}
+                            autoCapitalize="none"
+                        />
+                        {errors.password && <Text style={styles.error}>{errors.password.message}</Text>}
+                    </>
+                )}
+            />
+
+
+            <TouchableOpacity style={styles.button} onPress={handleSubmit(onSubmit)}>
+                <Text style={{ color: "#fff", fontWeight: "500" }}>Cadastrar-se</Text>
+            </TouchableOpacity>
         </View>
     );
 }
@@ -12,7 +118,31 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: "center",
-        justifyContent: "space-between",
-        paddingVertical: 40
+        justifyContent: "center",
+        paddingVertical: 40,
+        paddingHorizontal: 20,
+        gap: 10
     },
+    button: {
+        backgroundColor: "#593C9D",
+        borderRadius: 5,
+        paddingVertical: 10,
+        color: "#fff",
+        width: "100%",
+        alignItems: "center",
+        marginTop: 50,
+    },
+    input: {
+        backgroundColor: "#fff",
+        padding: 10,
+        width: "100%",
+        borderWidth: 1,
+        borderRadius: 5,
+    },
+    error: {
+        color: 'red',
+        alignSelf: 'flex-start',
+        fontSize: 10,
+        fontWeight: '500'
+    }
 });
